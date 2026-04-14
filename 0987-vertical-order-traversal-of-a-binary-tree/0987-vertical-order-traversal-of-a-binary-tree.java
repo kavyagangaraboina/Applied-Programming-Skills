71e312580e8344010f276f0b6508117d1d97a6c7
kavya <gangaraboinakavya16@gmail.com>
2026-04-14 17:16:17 +0530

    Time: 3 ms (94.7%), Space: 43.9 MB (80.92%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,20 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        
+    public List<List<Integer>> verticalTraversal(TreeNode r) {
+        List<int[]> l = new ArrayList<>();
+        dfs(r, 0, 0, l);
+        Collections.sort(l, (a,b) -> 
+            a[0]==b[0] ? (a[1]==b[1] ? a[2]-b[2] : a[1]-b[1]) : a[0]-b[0]);
+        List<List<Integer>> res = new ArrayList<>();
+        int prev = Integer.MIN_VALUE;
+        for (int[] x : l) {
+            if (x[0] != prev) {
+                res.add(new ArrayList<>());
+                prev = x[0];
+            }
+            res.get(res.size()-1).add(x[2]);
+        }
+        return res;
+    }
+
+    void dfs(TreeNode n, int c, int r, List<int[]> l) {
+        if (n == null) return;
+        l.add(new int[]{c, r, n.val});
+        dfs(n.left, c-1, r+1, l);
+        dfs(n.right, c+1, r+1, l);
     }
 }
