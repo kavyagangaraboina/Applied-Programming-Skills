f9e2223a6cce6968d0717e5ebb66094b0be20daf
kavya <gangaraboinakavya16@gmail.com>
2026-05-07 23:44:11 +0530

    Time: 24 ms (33.58%), Space: 47.5 MB (28.45%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,22 +1,43 @@
+import java.util.*;
+
 class Solution {
+
     public String minRemoveToMakeValid(String s) {
-        char[] a = s.toCharArray();
-        int c = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == '(') c++;
-            else if (a[i] == ')') {
-                if (c == 0) a[i] = '*';
-                else c--;
+
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> remove = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            }
+
+            else if (ch == ')') {
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    remove.add(i);
+                }
             }
         }
-        for (int i = a.length - 1; i >= 0 && c > 0; i--) {
-            if (a[i] == '(') {
-                a[i] = '*';
-                c--;
+
+        while (!stack.isEmpty()) {
+            remove.add(stack.pop());
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (!remove.contains(i)) {
+                result.append(s.charAt(i));
             }
         }
-        StringBuilder r = new StringBuilder();
-        for (char ch : a) if (ch != '*') r.append(ch);
-        return r.toString();
+
+        return result.toString();
     }
 }
