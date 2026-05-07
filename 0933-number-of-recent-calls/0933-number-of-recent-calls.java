db01c34b3c5d26203c6daa7662643bb1f57989d2
kavya <gangaraboinakavya16@gmail.com>
2026-05-07 23:40:24 +0530

    Time: 22 ms (56.17%), Space: 59.6 MB (50.57%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,18 +1,17 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class RecentCounter {
 
-    private Queue<Integer> queue;
+    Queue<Integer> queue;
 
     public RecentCounter() {
         queue = new LinkedList<>();
     }
 
     public int ping(int t) {
-        queue.add(t);
 
-        // Remove old requests
+        queue.offer(t);
+
         while (queue.peek() < t - 3000) {
             queue.poll();
         }
