8210f4ca9e56fe5f72d8700922063c5ede4865e4
kavya <gangaraboinakavya16@gmail.com>
2026-05-07 23:33:58 +0530

    Time: 0 ms (100%), Space: 43.6 MB (52.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,32 +1,37 @@
-class TreeNode {
-    int val;
-    TreeNode left, right;
-
-    TreeNode(int val) {
-        this.val = val;
-        left = right = null;
-    }
-}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
 class Solution {
+
     public boolean isSymmetric(TreeNode root) {
         return isMirror(root.left, root.right);
     }
 
-    public boolean isMirror(TreeNode left, TreeNode right) {
-        // Case 1: both null
-        if (left == null && right == null) {
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null && t2 == null) {
             return true;
         }
 
-        // Case 2: one null
-        if (left == null || right == null) {
+        if (t1 == null || t2 == null) {
             return false;
         }
 
-        // Case 3: check value + mirror condition
-        return (left.val == right.val) &&
-               isMirror(left.left, right.right) &&
-               isMirror(left.right, right.left);
+        return (t1.val == t2.val)
+                && isMirror(t1.left, t2.right)
+                && isMirror(t1.right, t2.left);
     }
 }
